e1275db8a364273cc90df859a761880e53ee1513
mthsaguiar <matheusaguiardev@gmail.com>
2022-03-20 16:29:44 -0300

    new shopify calls

--- includes/shopify.php
+++ includes/shopify.php
@@ -31,6 +31,49 @@ class Shopify {
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($curl, CURLOPT_MAXREDIRS, 3);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+
+        $headers[] = "";
+        if(!is_null($this->access_token)){
+            $headers[] = "X-Shopify-Access-Token: " . $this->access_token;
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        }
+        if($method != "GET" && in_array($method, array('POST', 'PUT'))){
+            if(is_array($query)) $query = http_build_query($query);
+            curl_setopt($curl, CURL_POSTFIELDS, $query);
+        }
+
+        $response = curl_exec($curl);
+        $error = curl_errno($curl);
+        $error_msg = curl_error($curl);
+        
+        curl_close($curl);
+
+        if($error){
+            return $error_msg;
+        } else{
+            $response = preg_split("/\r\n\r\n|\n\n|\r\r/", $response, 2);
+
+            // echo print_r($response);
+
+            $headers = array();
+            $headers_content = explode("\n", $response[0]);
+            $headers['status'] = $headers_content[0];
+
+            array_shift($headers_content);
+
+            foreach($headers_content as $content){
+                $data = explode(':', $content);
+                $headers[trim( $data[0] )] = trim( $data[1] );
+
+            }
+
+            return array('headers' => $headers, 'body' => $response[1]);
+        }
         
     }
 }
