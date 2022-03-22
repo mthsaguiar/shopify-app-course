27c32b81fefb50a64e61f05d4a0bc31fb7e43946
mthsaguiar <matheusaguiardev@gmail.com>
2022-03-22 19:38:22 -0300

    app layout

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 include_once("includes/mysql_connect.php");
 include_once("includes/shopify.php");
 
-$shopify = new Shopify();
+/**
+*===========================================
+*        CREATE THE VARIABLES:
+*            - $shopify
+*            - $parameters
+*===========================================
+*/
 
+$shopify = new Shopify();
 $parameters = $_GET;
 
 /**
@@ -12,29 +19,7 @@ $parameters = $_GET;
 *===========================================
 */
 
-$query = "SELECT * FROM shops WHERE shop_url='" . $parameters['shop'] . "' LIMIT 1";
-$result = $mysql -> query($query);
-
-if($result -> num_rows < 1){
-    header("Location: install.php?shop=" . $_GET['shop']);
-    exit();
-}
-
-$store_data = $result -> fetch_assoc();
-
-echo $store_data['access_token'];
-
-
-$shopify->set_url($parameters['shop']);
-$shopify->set_token($store_data['access_token']);
-
-$shop = $shopify->rest_api('/admin/api/2022-01/shop.json', array(), 'GET');
-$response = json_decode($shop['body'], true);
-
-if(array_key_exists('errors', $response)) {
-    header("Location: install.php?shop=" . $_GET['shop']);
-    exit();
-}
+include_once("includes/check_token.php");
 
 /**
 *===========================================
@@ -42,7 +27,26 @@ if(array_key_exists('errors', $response)) {
 *===========================================
 */
 
-$access_scopes = $shopify->rest_api('/admin/oauth/access_scopes.json', array(), 'GET');
-$response_access_scope = json_decode($access_scopes['body'], true);
+// $access_scopes = $shopify->rest_api('/admin/oauth/access_scopes.json', array(), 'GET');
+// $response_access_scope = json_decode($access_scopes['body'], true);
 
-echo print_r($response_access_scope);
+?>
+
+<?php include_once("header.php"); ?>
+
+    <section>
+        <div class="alert columns twelve">
+            <dl>
+                <dt>
+                    <p>Welcome to our app.</p>
+                </dt>
+            </dl> 
+        </div>
+    </section>
+    <footer>
+
+    </footer>
+
+
+
+<?php include_once("footer.php"); ?>
